8d8b68b4ee01cf16692a68a10e8219e80c7da44e
Martin Blackburn <walking_fishy@hotmail.com>
2013-07-27 19:25:00 +0100

    player now moves

--- js/tweetDefendor.js
+++ js/tweetDefendor.js
@@ -3,12 +3,22 @@ TweetDefendor = function()
     //variables
     var finalSentence = "";
     var gameboardHeight = 0;
+    var player = $(".player");
     
     resizeGameBoard();
     
     //listener for screen resize
 	$(window).resize(function() {
 		resizeGameBoard();
+	});
+	
+	//make the player follow the mouse (start when mouse is over it)
+	$(".gameBoard").on("mousemove", function(event) {
+		if(event.offsetY < gameboardHeight - player.height()) {
+		    player.css({
+		       top: event.offsetY
+		    });
+		}
 	});
     
     //resize gameboard
