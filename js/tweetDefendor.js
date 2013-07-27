9ac23d71780db9941beaee8f4b5ebca016289342
Martin Blackburn <walking_fishy@hotmail.com>
2013-07-27 15:32:53 +0100

    basic js functions

---
+++ js/tweetDefendor.js
@@ -0,0 +1,31 @@
+TweetDefendor = function() 
+{   
+	//ebooks array
+	var ebooks = "";
+	
+    //varialbles
+	var words = "";
+	
+	//get random words, for first level
+    function randomWords()
+    {   
+        
+    }
+    
+    //get next words base on previous word
+    function getNextWords()
+    {
+    	
+    }
+    
+    //clear screen of words
+    function clearScreen()
+    {
+    	
+    }
+};
+
+$(function() 
+{
+    new TweetDefendor();
+});
