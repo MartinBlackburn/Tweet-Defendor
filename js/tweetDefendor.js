28b6b08b5165d2d79400bc3a20dd44ef0a070895
Jonny Blackburn <mrloopy2@hotmail.com>
2013-07-28 13:55:06 +0100

    Combined markov and tweetDefendor files.

--- js/tweetDefendor.js
+++ js/tweetDefendor.js
@@ -4,6 +4,27 @@ TweetDefendor = function()
     var finalSentence = "";
     var gameboardHeight = 0;
     var player = $(".player");
+
+    //List of url for source text
+    var mobyDick = "text/mobyDick.txt";
+    var frankenstein = "text/frankenstein.txt";
+    var alice = "text/alice.txt";
+
+    //ebooks array
+    var ebookList = [mobyDick, alice, frankenstein];
+    var finalSourceText = "";
+
+    //Array of all the words
+    var wordArray;
+
+    //Array of words that can appear on screen
+    var wordBuffer = [];
+
+    //chosen word
+    var chosenWord = null;
+    
+    //final sentence
+    var finalSentence = "";
     
     resizeGameBoard();
     
@@ -34,6 +55,94 @@ TweetDefendor = function()
     {
     	
     }
+
+    /*==============================================================
+                        Stuff for Morkov chain
+    ==============================================================*/
+
+    loadEbooks();
+
+    //Loop through the ebook array and load each ebook
+    function loadEbooks()
+    {
+        console.log("LoadEbook");
+
+        //Loop through the ebookList and load each ebook
+        for(var i = 0; i < ebookList.length; i++)
+        {
+            console.log("loading: " + ebookList[i]);
+            $.ajax({
+                url: ebookList[i],
+                crossDomain: true,
+                success: createSourceText,
+                error: function(xhr, error, text2){ console.log(xhr); console.log(error); console.log(text2); }
+            });
+        }
+    }
+
+    function createSourceText(data)
+    {
+        console.log("createSourceText");
+
+        finalSourceText = finalSourceText + "/n/n" + data;
+
+        wordArray = finalSourceText.split(/\s+/g);
+
+        stripWords();
+        
+        randomStartWords();
+    }
+
+    //Strip punctuation from words in word array
+    function stripWords()
+    {
+        for(var i = 0; i < wordArray.length; i++)
+        {
+            wordArray[i] = wordArray[i].replace(/\b[-.,;:()&$#!?\[\]{}"']+\B|\B[-.,;:()&$#!?\[\]{}"']+\b/g, "").toLocaleLowerCase();
+        }
+    }
+
+    //get random words, for first set of words
+    function randomStartWords()
+    {
+        console.log("RandomWords");
+
+        for(var i = 0; i < 50; i++)
+        {
+            var randomNum = Math.ceil(Math.random()*wordArray.length);
+
+            wordBuffer.push( wordArray[randomNum]);
+        }
+    }
+
+    //get next words base on the word passed in
+    function genNextWords()
+    {
+        console.log("genNextWords");
+        wordBuffer = [];
+
+        for(var i = 0; i < wordArray.length; i++)
+        {
+            if(wordArray[i] == chosenWord)
+            {
+                wordBuffer.push(wordArray[i+1]);
+            }
+        }
+    }
+
+    function getRandomWord()
+    {
+        console.log("getRandomWord");
+        return wordBuffer[Math.floor(Math.random()*wordBuffer.length)];
+    }
+    
+    function setWord(word)
+    {
+        chosenWord = word;
+        
+        genNextWords();
+    }
+};
 };
 
 $(function() 
