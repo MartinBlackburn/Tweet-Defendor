704e09fe307113a4014199683ff24bc30946a20c
Martin Blackburn <walking_fishy@hotmail.com>
2013-07-30 13:20:46 +0100

    added credit to index

--- index.php
+++ index.php
@@ -24,6 +24,33 @@
                 But usually it ends up just being jumbled garbage resembling spam.
 			</div>
 			
+			<div class="about">
+				<h3>Books</h3>
+				Moby Dick by Herman Melville <br />
+                Alice's Adventures in Wonderland by Lewis Carroll  <br />
+                Frankenstein by Mary Wollstonecraft Shelley  <br />
+                Pride and Prejudice by Jane Austen  <br />
+                Adventures of Huckleberry Finn by Mark Twain  <br />
+                Wuthering Heights by Emily Bronte  <br />
+                Peter Pan by J. M. Barrie  <br />
+                The Importance of Being Earnest by Oscar Wilde  <br />
+                A Tale of Two Cities by Charles Dickens  <br />
+                Little Brother by Cory Doctorow  <br />
+                FTW by Cory Doctorow  <br />
+                Printcrime by Cory Doctorow  <br />
+                When Sysadmins Ruled the Earth by Cory Doctorow  <br />
+                I, Robot by Cory Doctorow<br />
+                <br />
+                
+                <h3>Hosting</h3>
+                <a href="http://labelmedia.co.uk">Label Media</a> <br />
+                <br />
+                
+                <h3>Other</h3>
+                <a href="http://www.gutenberg.org/wiki/Main_Page">Project Gutenberg (free ebooks)</a> <br />
+				<a href="https://github.com/mynetx/codebird-php">CodeBird (Twitter auth library) by mynetx</a>
+			</div>
+			
 			<a class="button play" href="/game.php">Play</a>
 		</div>
 	</body>
